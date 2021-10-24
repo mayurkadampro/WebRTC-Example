ce8f142d872477f9536d05d7fc09bd0d8eeefec1
Mayur Kadam <mayurkadampro>
2021-10-24 20:54:12 +0530

    added peer js connection for heroku app

--- public/script.js
+++ public/script.js
@@ -1,8 +1,9 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-  host: '/',
-  port: '3001'
+  secure: true,
+  host: 'https://webrtc-check.herokuapp.com/',
+  port: 443,
 })
 const myVideo = document.createElement('video')
 myVideo.muted = true
